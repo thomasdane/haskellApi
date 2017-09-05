f9ff4d31e735c47283697ff7f625f06e907791f5
Tom Dane <tjdane@gmail.com>
2017-09-05 18:31:13 +0100

    Chapter 3 of practical haskell

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,14 @@ module Main where
 import System.IO (readFile)
 import Data.Time (getCurrentTime)
 import Data.Aeson
+import RepeatMessage
 
 main :: IO ()
 main = do
+ sayHello
+
+sayHello :: IO ()
+sayHello = do
  putStrLn "What is your name"
  input <- getLine
  putStrLn $ greet input
@@ -14,18 +19,24 @@ greet name = "Hello " ++ name
 
 printConfig :: IO ()
 printConfig = do
-	contents <- readFile "stack.yaml"
-	putStrLn contents
+ contents <- readFile "stack.yaml"
+ putStrLn contents
 
 printTime :: IO ()
 printTime = do
-	time <- getCurrentTime
-	putStrLn $ show time
+ time <- getCurrentTime
+ putStrLn $ show time
 
 numbers :: [Int]
 numbers = [1,2,3,4]
 
 encodeToJson = do
-	encode numbers
+ encode numbers
+
+beCareful :: Maybe Int
+beCareful = do
+ Just 6
+ Nothing
+ return 5 
 
 
